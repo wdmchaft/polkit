db71ec886c13be196d26dad28c65f2c95017d5d9
info@pol-online.net <info@pol-online.net@d0d7bc14-f39f-11dd-87c9-111d11f6bf44>
2009-03-19 19:14:07 +0000

    Fix

--- FileTransferController/FileTransferController_FTP.m
+++ FileTransferController/FileTransferController_FTP.m
@@ -121,7 +121,7 @@ static int _DebugCallback(CURL* handle, curl_infotype type, char* data, size_t s
 	curl_easy_setopt(_handle, CURLOPT_FORBID_REUSE, (long)(_keepAlive ? 0 : 1));
 	curl_easy_setopt(_handle, CURLOPT_IPRESOLVE, (long)CURL_IPRESOLVE_V4); //HACK: Work around an issue with Bonjour hostnames that resolve to IPv6 and passive connections can't be established
 	if(timeOut > 0.0)
-	curl_easy_setopt(_handle, CURLOPT_TIMEOUT, (long)ceil(timeOut));
+	curl_easy_setopt(_handle, CURLOPT_FTP_RESPONSE_TIMEOUT, (long)ceil(timeOut));
 	
 	curl_easy_setopt(_handle, CURLOPT_VERBOSE, (long)1);
 	curl_easy_setopt(_handle, CURLOPT_DEBUGFUNCTION, _DebugCallback);
